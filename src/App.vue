601be5d4f805bfaa74792c306e21f97dabff611e
KWilson718 <wilsonk18@spu.edu>
2024-07-02 19:06:09 -0700

    Basic Layout & Favicon Added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-import HomePage from './components/HomePage.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import HomePageComp from './components/HomePageComp.vue'
+import FooterComp from './components/FooterComp.vue'
 </script>
 
 <template>
-  <HomePage/>
+  <HeaderComp/>
+  <HomePageComp/>
+  <FooterComp/>
 </template>
 
 <style scoped>
