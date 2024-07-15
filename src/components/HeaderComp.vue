92ec3c2c9319a8566eab38369ebae346c26694f6
KWilson718 <wilsonk18@spu.edu>
2024-07-15 14:35:30 -0700

    Slight formatting tweaks

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -4,7 +4,25 @@
             <img id="logo" src="../assets/favicon/android-chrome-512x512.png" alt="Current Logo">
         </div>
         <div id="middle">
-            <h2>Home | About | Experience | Projects | Resume | Misc</h2>
+            <!-- <h2>Home | About | Experience | Projects | Resume | Misc</h2> -->
+            <div class="navButton">
+                <h2>Home</h2>
+            </div>
+            <div class="navButton">
+                <h2>About</h2>
+            </div>
+            <div class="navButton">
+                <h2>Experience</h2>
+            </div>
+            <div class="navButton">
+                <h2>Projects</h2>
+            </div>
+            <div class="navButton">
+                <h2>Resume</h2>
+            </div>
+            <div class="navButton">
+                <h2>Misc</h2>
+            </div>
         </div>
         <div id="rightBumper"></div>
     </div>
@@ -23,15 +41,24 @@
         flex: 1;
     }
     #middle{
-        flex: 5;
+        flex: 3;
         text-align: center;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
     #rightBumper{
         flex: 1;
     }
 
+    /* Formatting the Main Nav Buttons */
+    .navButton{
+        flex: 1;
+    }
+
     /* Handles sizing of the logo image to the top left */
     #logo{
+        padding: .5rem;
         width: 6rem;
         height: 6rem;
     }
