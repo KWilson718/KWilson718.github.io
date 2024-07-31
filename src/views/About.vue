0ed53906db9c78cd796f15714d43c83692e3e4e2
KWilson718 <wilsonk18@spu.edu>
2024-07-31 16:19:18 -0700

    Adding in more info to About

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,33 @@
     <div class="about">
         <h2>Who I Am</h2>
         <p>Currently, I am a student at Seattle Pacific University, studying Computer Science & serving as a Resident Advisor in Residence Life</p>
+        <p>At around the end of 5th Grade I started getting interested in programming, and knew I wanted to make a career out of it by the end of Middle School</p>
+        <p>I have participated in multiple smaller projects, and am now working on building out a portfolio to document what I have learned</p>
         <h2>What Have I Done</h2>
+        <p>More specific information can be found in the Experience & Projects pages, but I have been able to participate in multiple smaller projects, am working an internship currently, and am gearing up to a Senior Year of tons of developmental experience</p>
         <h2>Goals for the Future</h2>
+        <p>All of that being said, I hope to move into a Software Engineering job to start out a hopefully long term career.</p>
+        <p>Currently, to gain further experience, I am planning on dabbling in the relating fields</p>
+        <div id="goalsDiv">
+            <ul id="goalsList">
+                <li>Continuing to understand more about Vue.js</li>
+                <li>Experimenting with Python, Rust, & C# to broaden the languages I can work with</li>
+                <li>Learning more about GitHub's features with checks, as well as running test cases in general</li>
+            </ul>
+        </div>
+        <p>As an aside, if you are interested in working on anything, please let me know, I'd love to collaborate!</p>
+        
     </div>
 </template>
+
+<style scoped>
+#goalsDiv {
+    text-align: center;
+}
+#goalsList {
+    display: inline-block;
+    list-style-type: square;
+    text-align: left;
+    padding-left: 20px;
+}
+</style>
