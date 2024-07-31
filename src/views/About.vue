fcd231e671aaaa4c44f5fc6eebc798a26fbba683
KWilson718 <wilsonk18@spu.edu>
2024-07-31 15:09:16 -0700

    Laying out basic idea of what will be held in the About Section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="about">
-        <h3>About</h3>
+        <h2>Who I Am</h2>
+        <p>Currently, I am a student at Seattle Pacific University, studying Computer Science & serving as a Resident Advisor in Residence Life</p>
+        <h2>What Have I Done</h2>
+        <h2>Goals for the Future</h2>
     </div>
 </template>
