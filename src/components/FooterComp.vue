5c5948e020b4d16b027675410881fc11ea12381a
KWilson718 <wilsonk18@spu.edu>
2024-07-31 13:27:55 -0700

    Updated Footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,15 +3,30 @@
         <table class="footerTable">
             <thead>
                 <tr>
-                    <td><p><b>Social Links</b></p></td>
+                    <td>
+                        <p class="tableText tableHeaderText"><b>Creation Information</b></p>
+                    </td>
+                    <td>
+                        <p class="tableText tableHeaderText"><b>Social Links</b></p>
+                    </td>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td><a href="https://github.com/KWilson718" class="socialLink">GitHub</a></td>
+                    <td>
+                        <p class="tableText">Created Using Vue.js 3 + Vite</p>
+                    </td>
+                    <td>
+                        <a href="https://github.com/KWilson718" class="socialLink">GitHub</a>
+                    </td>
                 </tr>
                 <tr>
-                    <td><a href="https://www.linkedin.com/in/wilson--kyle/" class="socialLink">LinkedIn</a></td>
+                    <td>
+                        <p class="tableText">Hosted using GitHub Pages</p>
+                    </td>
+                    <td>
+                        <a href="https://www.linkedin.com/in/wilson--kyle/" class="socialLink">LinkedIn</a>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -41,9 +56,23 @@
 }
 
 /* Table Structure */
-.footerTable thead tr td p{
+.footerTable{
+    width: 100%;
+}
+.footerTable thead tr td, .footerTable tbody tr td{
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+
+/* Text Coloring */
+
+.tableHeaderText{
+    border-bottom: 2px solid rgb(90, 90, 90);
+}
+.tableText{
     padding: 0;
     margin: 0;
+    color: white;
 }
 
 /* Link Handling */
